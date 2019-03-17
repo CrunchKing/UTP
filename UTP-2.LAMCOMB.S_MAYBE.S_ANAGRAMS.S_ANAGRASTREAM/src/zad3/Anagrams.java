06fb1a29c257759a5a5fc19d68b245aee763e7e9
Shaquu <tadev3@gmail.com>
2019-03-17 10:15:20 +0100

    Init UTP2

---
+++ UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad3/Anagrams.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Wyrzykowski Tadeusz S17315
+ *
+ */
+
+package zad3;
+
+
+public class Anagrams {
+}  
---
+++ UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad4/Main.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *  @author Wyrzykowski Tadeusz S17315
+ *
+ */
+
+package zad4;
+
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
