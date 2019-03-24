5a798ecd1d5cea8dafb50c6cb0976d1ce29340da
Shaquu <tadev3@gmail.com>
2019-03-24 23:37:41 +0100

    UTP2.3

--- UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad3/Anagrams.java
+++ UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad3/Anagrams.java
@@ -7,5 +7,70 @@
 package zad3;
 
 
-public class Anagrams {
-}  
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+class Anagrams {
+
+    private List<String> words = new ArrayList<>();
+    private HashMap<String, List<String>> anagrams = new HashMap<>();
+
+    private static String sortChars(String input)
+    {
+        char[] output = input.toCharArray();
+        Arrays.sort(output);
+        return new String(output);
+    }
+
+    Anagrams(String fileName) {
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(l -> words.addAll(Arrays.asList(l.split("\\s+"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String word : words) {
+            String sorted = sortChars(word);
+
+            if (anagrams.containsKey(sorted)) {
+                anagrams.get(sorted).add(word);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                anagrams.put(sorted, list);
+            }
+        }
+    }
+
+    String getAnagramsFor(String value) {
+        String key = sortChars(value);
+        String anagramsFlat= "";
+
+        if (anagrams.containsKey(key)) {
+            anagramsFlat = anagrams.get(key).stream().filter(a -> !a.equals(value)).collect(Collectors.joining(", "));
+        }
+
+        return value + ": [" + anagramsFlat + "]";
+    }
+
+    Iterable<List<String>> getSortedByAnQty() {
+        return anagrams.entrySet().stream()
+                .sorted((o1, o2) -> {
+                    int o1s = o1.getValue().size();
+                    int o2s = o2.getValue().size();
+                    return Integer.compare(o2s, o1s);
+                })
+                .collect(toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a,b) -> {throw new AssertionError();},
+                        LinkedHashMap::new
+                )).values();
+    }
+}
