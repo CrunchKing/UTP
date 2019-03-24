5b241ed69b7c2579827788933f4eaff127b0eb59
Shaquu <tadev3@gmail.com>
2019-03-24 22:32:48 +0100

    Minor improvement to Generics

--- UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad1/InputConverter.java
+++ UTP-2.LAMCOMB.S_MAYBE.S_ANAGRAMS.S_ANAGRASTREAM/src/zad1/InputConverter.java
@@ -10,12 +10,12 @@ public class InputConverter<T> {
         this.t = t;
     }
 
-    public <A, B> B convertBy(Function... functions) {
+    public <B> B convertBy(Function... functions) {
         Object input = t;
         Object result = null;
 
-        for (Function<A, B> f : functions) {
-            result = f.apply((A) input);
+        for (Function f : functions) {
+            result = f.apply(input);
             input = result;
         }
 
